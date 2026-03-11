739f835c045782332d34a9c72d169d766f35655b
Lucas Cabrio <lucascabrio52@gmail.com>
2026-03-11 20:56:24 -0300

    atividade shell script organizador de arquivos

---
+++ organizacao/codigos/organizador.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+contador=0
+
+mkdir -p organizacao/textos
+mkdir -p organizacao/imagens
+mkdir -p organizacao/codigos
+mkdir -p organizacao/outros
+
+for arquivo in *; do
+
+    if [ -f "$arquivo" ]; then
+
+        case "$arquivo" in
+
+            *.txt|*.pdf)
+                mv "$arquivo" organizacao/textos/
+                contador=$((contador+1))
+                ;;
+
+            *.jpg|*.png)
+                mv "$arquivo" organizacao/imagens/
+                contador=$((contador+1))
+                ;;
+
+            *.c|*.sh)
+                mv "$arquivo" organizacao/codigos/
+                contador=$((contador+1))
+                ;;
+
+            *)
+                mv "$arquivo" organizacao/outros/
+                contador=$((contador+1))
+                ;;
+
+        esac
+
+    fi
+
+done
+
+echo "Organização concluída com sucesso!"
+echo "Total de arquivos movidos: $contador"
